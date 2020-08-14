710af5b9dc1bd51494818b717cc96879e8f3417c
edouard goisbault <edouard.goisbault01@gmail.com>
2020-08-14 12:06:27 +0200

    last register

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "mezcal")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "tonic")
+Ingredient.create(name: "Whisky")
+Ingredient.create(name: "cucumber")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Tabasco")
